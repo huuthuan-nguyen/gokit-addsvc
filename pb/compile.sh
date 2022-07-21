5a46f7e48f896fda98fbbb7e9000c12254459654
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2022-07-21 14:45:47 +0700

    update proto file

--- pb/compile.sh
+++ pb/compile.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 
-# Install proto3 from source
-# brew install autoconf automake libtool
-# git clone https://github.com/google/protobuf
-# ./autogen.sh; ./configure; make; make install
+# Install proto3 from brew
+# brew install protobuf
 #
 # update protoc Go bindings via
-# go get -u github.com/golang/protobuf/{proto,protoc-gen-go}
+# go get -u google.golang.org/protobuf/cmd/protoc-gen-go
+# go install google.golang.org/protobuf/cmd/protoc-gen-go
 #
+# go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc
+# go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
 # See also
 # https://github.com/grpc/grpc-go/tree/master/examples
 
-protoc addsvc.proto --go_out=plugins=grpc:.
+protoc addsvc.proto --go_out=. --go-grpc_out=.
