1b18c27a6af80c8ce7b077384d53e53cec485849
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2022-07-22 19:24:15 +0700

    WIP: add service command

--- cmd/addsvc/addsvc.go
+++ cmd/addsvc/addsvc.go
@@ -1,1 +1,14 @@
-package addsvc
+package main
+
+import "flag"
+
+func main() {
+	fs := flag.NewFlagSet("addsvc", flag.ExitOnError)
+	var (
+		debugAddr   = fs.String("debug.addr", ":8080", "Debug and metrics listen address")
+		httpAddr    = fs.String("http-addr", ":8081", "HTTP listen address")
+		grpcAddr    = fs.String("grpc-addr", ":8082", "gRPC listen address")
+		thriftAddr  = fs.String("thrift-addr", ":8083", "Thrift listen address")
+		jsonRPCAddr = fs.String("jsonrpc-addr", ":8084", "JSON RPC listen address")
+	)
+}
